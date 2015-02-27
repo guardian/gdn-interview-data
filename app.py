9153154540ffa66e6ddce2d4ef1fef68b1ca8f7a
Robert Rees <robert.rees@guardian.co.uk>
2015-02-27 11:22:40 +0000

    Allows the creation of a candidate

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ import os
 import json
 import logging
 from urllib import quote, urlencode
+
 from google.appengine.api import urlfetch
+from google.appengine.ext import ndb
+
+import models
 
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), "templates")))
@@ -34,20 +38,31 @@ class Interview(webapp2.RequestHandler):
 		self.response.out.write(template.render(template_values))
 
 class Candidate(webapp2.RequestHandler):
-	def get(self):
+	def get(self, key):
 		template = jinja_environment.get_template('candidates/individual.html')
 		
-		template_values = {}
+		template_values = {
+			'candidate': ndb.Key(urlsafe=key).get(),
+		}
 
 		self.response.out.write(template.render(template_values))
 
 class NewCandidate(webapp2.RequestHandler):
 	def get(self):
-		template = jinja_environment.get_template('candidates/new.html')
+		template = jinja_environment.get_template('candidates/add.html')
 		
 		template_values = {}
 
 		self.response.out.write(template.render(template_values))
+
+	def post(self):
+
+		candidate = models.Candidate(name=self.request.get('name'),
+			role=self.request.get('role'))
+
+		candidate.put()
+
+		return webapp2.redirect('/candidate/{0}'.format(candidate.key.urlsafe()))
 
 class AllCandidates(webapp2.RequestHandler):
 	def get(self):
@@ -62,7 +77,7 @@ app = webapp2.WSGIApplication([
 	webapp2.Route(r'/', handler=MainPage),
 	webapp2.Route(r'/dashboard', handler=Dashboard),
 	webapp2.Route(r'/interview', handler=Interview),
-	webapp2.Route(r'/candidate', handler=Candidate),
-	webapp2.Route(r'/candidates', handler=AllCandidates),
 	webapp2.Route(r'/candidate/new', handler=NewCandidate),
+	webapp2.Route(r'/candidate/<key>', handler=Candidate),
+	webapp2.Route(r'/candidates', handler=AllCandidates),
 	], debug=True)
--- models.py
+++ models.py
@@ -3,3 +3,8 @@ from google.appengine.ext import ndb
 class Configuration(ndb.Model):
 	key = ndb.StringProperty(required=True)
 	value = ndb.StringProperty(required=True)
+
+
+class Candidate(ndb.Model):
+	name = ndb.StringProperty(required=True)
+	role = ndb.StringProperty(required=True)
