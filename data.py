bc8269e97a66baa810d96b608a437c7e70430b96
Robert Rees <robert.rees@guardian.co.uk>
2016-02-13 23:57:17 +0000

    Create a data module as part of starting to data-drive the logic

---
+++ data.py
@@ -0,0 +1,16 @@
+
+interview_outcomes = [
+	"Proceed",
+	"Reject",
+]
+
+outcomes = [
+	"Declined",
+	"Offer made",
+	"Offer accepted",
+	"Offer rejected",
+]
+
+unresolved_outcomes = [
+	"Offer made",
+]
